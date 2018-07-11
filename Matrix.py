e52a115a5cbffb265670a2f971e381f2f05beb1b
Plockwurst <pelle.ofenbach@tu-dortmund.de>
2018-07-11 13:13:22 +0200

    Initialising V14 	modified:   Matrix.py 	new file:   V14-Topografie/.gitignore 	new file:   V14-Topografie/Makefile 	new file:   V14-Topografie/content/auswertung.tex 	new file:   V14-Topografie/content/diskussion.tex 	new file:   V14-Topografie/content/durchfuehrung.log 	new file:   V14-Topografie/content/durchfuehrung.tex 	new file:   V14-Topografie/content/theorie.tex 	new file:   V14-Topografie/header-matplotlib.tex 	new file:   V14-Topografie/header.tex 	new file:   V14-Topografie/lit.bib 	new file:   V14-Topografie/logos/FFSLogo.jpeg 	new file:   V14-Topografie/logos/tu_logo.jpg 	new file:   V14-Topografie/main.tex 	new file:   V14-Topografie/matplotlibrc 	new file:   V14-Topografie/plots/plot.py 	new file:   V14-Topografie/programme.bib

--- Matrix.py
+++ Matrix.py
@@ -7,9 +7,17 @@ from uncertainties import correlated_values, correlation_matrix, ufloat
 import uncertainties.unumpy as unp
 
 
-A = np.matrix([[4,1,1,1,2,0,1,0,2],[1,6,1,2,1,2,0,1,0],[1,1,4,0,2,1,2,0,1],[1,2,0,6,1,1,1,2,0],[2,1,2,1,6,1,2,1,2],[0,2,1,1,1,6,0,2,1],[1,0,2,1,2,0,4,1,1],[0,1,0,2,1,2,1,6,1],[2,0,1,0,2,1,1,1,4]])
-
-print('ATA=',A)
-
-A= np.linalg.inv(A)
+A = np.matrix([[0,np.sqrt(2),0,np.sqrt(2),0,0,0,0,0],[0,0,np.sqrt(2),0,np.sqrt(2),0,np.sqrt(2),0,0],[0,0,0,0,0,np.sqrt(2),0,np.sqrt(2),0],[1,1,1,0,0,0,0,0,0],[0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,1,1,1],[0,np.sqrt(2),0,0,0,np.sqrt(2),0,0,0],[np.sqrt(2),0,0,0,np.sqrt(2),0,0,0,np.sqrt(2)],[0,0,0,np.sqrt(2),0,0,0,np.sqrt(2),0],[0,0,1,0,0,1,0,0,1],[0,1,0,0,1,0,0,1,0],[1,0,0,1,0,0,1,0,0]])
 print('A=',A)
+
+ATA = np.matrix.dot(np.matrix.transpose(A),A)
+print('ATA=', ATA)
+print('ATA^-1=', np.linalg.inv(ATA))
+print('varianzfortpflanzung: ', np.diagonal(np.linalg.inv(ATA)))
+C = 0.03*np.eye(9,9)
+print('C=', C)
+sigma =  np.matrix.dot(ATA,C)
+print('sigam=',sigma)
+
+N=(np.diagonal(sigma))**(-2)
+print('N=',N)
